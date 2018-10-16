8f6c03515b8f6d6c76dc30692da9b085ebea5a6f
B2B\ansirm <mohammed.ansir@reedexpo.co.uk>
2018-10-16 09:56:09 +0100

    work sync

--- node-express-server/server.js
+++ node-express-server/server.js
@@ -1,27 +1,40 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials((__dirname + '/views/partials'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
 app.get('/', (request, response) => {
     //response.send('<h1>Hello express!</h1>');
-    response.send({
-        name: 'Lenny',
-        likes: [
-            'Bikes',
-            'PS4',
-            'Travel'
-        ]
+    response.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to my website'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About'
+    });
 });
 
 app.get('/bad', (req, res) => {
     res.send({
-        errorMessage: 'There has been an error!!!'
+        errorMessage: 'There has been an error'
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up');
+});
